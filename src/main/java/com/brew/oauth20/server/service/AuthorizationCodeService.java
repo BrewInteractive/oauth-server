cfc852968897e7fa8d7f999b3b50429c22fb94a3
Mehmet Fahri Özmen <mehmetfahriozmen@gmail.com>
2023-03-07 18:39:58 +0300

    refactor: code cleaned up.

--- src/main/java/com/brew/oauth20/server/service/AuthorizationCodeService.java
+++ src/main/java/com/brew/oauth20/server/service/AuthorizationCodeService.java
@@ -1,6 +1,7 @@
 package com.brew.oauth20.server.service;
 
 public interface AuthorizationCodeService {
-    String createAuthorizationCode(String subject,String redirectUri,long expiresIn,int clientId);
-    String getAuthorizationCode(String code,String redirectUri,boolean markAsUsed);
+    String createAuthorizationCode(String subject, String redirectUri, long expiresIn, int clientId);
+
+    String getAuthorizationCode(String code, String redirectUri, boolean markAsUsed);
 }
