cde1605bc70711bac00acc08eea5635d0cdee5a5
Mehmet Fahri Özmen <mehmetfahriozmen@gmail.com>
2023-03-14 15:14:23 +0300

    feat(TMID-34): Implemented ClientValidator.

--- src/main/java/com/brew/oauth20/server/utils/ClientValidator.java
+++ src/main/java/com/brew/oauth20/server/utils/ClientValidator.java
@@ -1,24 +1,47 @@
 package com.brew.oauth20.server.utils;
 
+import com.brew.oauth20.server.data.enums.ResponseType;
 import com.brew.oauth20.server.model.ClientModel;
 import com.brew.oauth20.server.model.ValidationResultModel;
 import com.brew.oauth20.server.utils.interfaces.Validator;
-
-import java.util.List;
 
 
 public class ClientValidator implements Validator<ClientModel> {
 
     private final String responseType;
-    private final List<String> redirectUriList;
+    private final String redirectUri;
 
-    public ClientValidator(String responseType, List<String> redirectUriList) {
+    public ClientValidator(String responseType, String redirectUri) {
         this.responseType = responseType;
-        this.redirectUriList = redirectUriList;
+        this.redirectUri = redirectUri;
+    }
+
+    private static ValidationResultModel getErrorResponse() {
+        return new ValidationResultModel(false, "unauthorized_client");
+    }
+
+    private static ValidationResultModel getSuccessResponse() {
+        return new ValidationResultModel(true, null);
     }
 
     @Override
     public ValidationResultModel validate(ClientModel client) {
-        return new ValidationResultModel(true, null);
+        if (!validateResponseType(client))
+            return getErrorResponse();
+
+        if (!validateRedirectUri(client))
+            return getErrorResponse();
+
+        return getSuccessResponse();
+    }
+
+    private boolean validateResponseType(ClientModel client) {
+        return client.grantList().stream()
+                .anyMatch(grantModel -> grantModel.responseType() == ResponseType.valueOf(this.responseType));
+    }
+
+    private boolean validateRedirectUri(ClientModel client) {
+        return client.redirectUriList().stream()
+                .anyMatch(redirectUriModel -> redirectUriModel.redirectUri().equals(this.redirectUri));
     }
 }
