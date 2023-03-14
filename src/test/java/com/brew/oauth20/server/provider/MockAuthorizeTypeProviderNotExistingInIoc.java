3b09530b022840b375066e64a4da70e24ab7a83c
erailea <toruna@gmail.com>
2023-03-14 22:07:38 +0300

    fix(TMID-35): fix service factory MissingServiceException case

--- src/test/java/com/brew/oauth20/server/provider/MockAuthorizeTypeProviderNotExistingInIoc.java
+++ src/test/java/com/brew/oauth20/server/provider/MockAuthorizeTypeProviderNotExistingInIoc.java
@@ -1,17 +1,10 @@
 package com.brew.oauth20.server.provider;
 
-import com.brew.oauth20.server.data.enums.ResponseType;
-import com.brew.oauth20.server.model.ValidationResultModel;
 import com.brew.oauth20.server.provider.AuthorizeType.BaseAuthorizeTypeProvider;
+import com.brew.oauth20.server.service.ClientService;
 
 public class MockAuthorizeTypeProviderNotExistingInIoc extends BaseAuthorizeTypeProvider {
-    @Override
-    public String getResponseType() {
-        return ResponseType.code.getResponseType();
-    }
-
-    @Override
-    public ValidationResultModel Validate(String clientId, String redirectUri) {
-        return new ValidationResultModel(true, "AuthorizeTypeProviderAuthorizationCode");
+    public MockAuthorizeTypeProviderNotExistingInIoc(ClientService clientService) {
+        super(clientService);
     }
 }
