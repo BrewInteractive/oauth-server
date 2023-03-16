2269ebad33c0e0d2936cc30113a479a6343bd131
Mehmet Fahri Özmen <mehmetfahriozmen@gmail.com>
2023-03-16 14:03:43 +0300

    refactor: renamed GrantType enum string.

--- src/main/java/com/brew/oauth20/server/data/enums/GrantType.java
+++ src/main/java/com/brew/oauth20/server/data/enums/GrantType.java
@@ -1,4 +1,24 @@
 package com.brew.oauth20.server.data.enums;
+
 public enum GrantType {
-    authorization_code
+    AUTHORIZATION_CODE("authorization_code");
+
+    private final String grantType;
+
+    GrantType(String grantType) {
+        this.grantType = grantType;
+    }
+
+    public static GrantType fromString(String grantTypeString) {
+        for (GrantType grantType : GrantType.values()) {
+            if (grantType.getResponseType().equals(grantTypeString)) {
+                return grantType;
+            }
+        }
+        return null;
+    }
+
+    public String getResponseType() {
+        return grantType;
+    }
 }
