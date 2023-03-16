48b3d4b186b78b43316286554d796d7152e3aeb1
erailea <toruna@gmail.com>
2023-03-16 19:17:53 +0300

    fix(): add test for Application.main

--- src/test/java/com/brew/oauth20/server/ApplicationTests.java
+++ src/test/java/com/brew/oauth20/server/ApplicationTests.java
@@ -5,17 +5,25 @@ import com.brew.oauth20.server.service.GreetingService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @SpringBootTest
 class ApplicationTests {
-	@Autowired
-	private GreetingController greetingController;
-	@Autowired
-	private GreetingService greetingService;
-	@Test
-	void contextLoads() {
-		assertThat(greetingController).isNotNull();
-		assertThat(greetingService).isNotNull();
-	}
+    @Autowired
+    private GreetingController greetingController;
+    @Autowired
+    private GreetingService greetingService;
+
+    @Test
+    void contextLoads() {
+        assertThat(greetingController).isNotNull();
+        assertThat(greetingService).isNotNull();
+    }
+
+    @Test
+    void main() {
+        assertDoesNotThrow(() -> Application.main(new String[]{}));
+    }
 }
