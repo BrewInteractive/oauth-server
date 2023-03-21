ddc388c6fe6cbf6a800741fafc527cb9dcf7af12
erailea <toruna@gmail.com>
2023-03-22 01:47:28 +0300

    fix(TMID-45): remove unnecessary exception

--- src/main/java/com/brew/oauth20/server/service/AuthorizationCodeService.java
+++ src/main/java/com/brew/oauth20/server/service/AuthorizationCodeService.java
@@ -1,7 +1,7 @@
 package com.brew.oauth20.server.service;
 
 public interface AuthorizationCodeService {
-    String createAuthorizationCode(Long userId, String redirectUri, long expiresIn, String clientId) throws Exception;
+    String createAuthorizationCode(Long userId, String redirectUri, long expiresIn, String clientId);
 
     String getAuthorizationCode(String code, String redirectUri, boolean markAsUsed);
 }
