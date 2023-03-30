d79c8ef4c741d2e6f77789aae49f9ad2c5cc9c12
erailea <toruna@gmail.com>
2023-03-30 23:56:12 +0300

    fix(TMID-45): fix regex as sonar suggests

--- src/main/java/com/brew/oauth20/server/utils/UriUtils.java
+++ src/main/java/com/brew/oauth20/server/utils/UriUtils.java
@@ -1,6 +1,5 @@
 package com.brew.oauth20.server.utils;
 
-import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
 
 public class UriUtils {
@@ -9,7 +8,7 @@ public class UriUtils {
 
     public static boolean isValidUrl(String url) throws PatternSyntaxException, NullPointerException {
         try {
-            return java.util.regex.Pattern.compile("\\b(?:(https?|ftp|file)://|www\\.)?[-A-Z0-9+&#/%?=~_|$!:,.;]*[A-Z0-9+&@#/%=~_|$]\\.[-A-Z0-9+&@#/%?=~_|$!:,.;]*[A-Z0-9+&@#/%=~_|$]", Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE).matcher(url).matches();
+            return java.util.regex.Pattern.compile("^(?i)(?:(?:https?|ftp|file)://|www\\.)[-\\w+&@#/%?=~_|!:,.;]*[-\\w+&@#/%=~_|]$").matcher(url).matches();
         } catch (PatternSyntaxException | NullPointerException ex) {
             return false;
         }
