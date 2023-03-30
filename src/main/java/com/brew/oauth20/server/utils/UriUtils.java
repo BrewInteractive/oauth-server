36a85d29264b1d08a2024e6213078bf904670020
erailea <toruna@gmail.com>
2023-03-30 23:43:12 +0300

    fix(TMID-45): fix regex as sonar suggests

--- src/main/java/com/brew/oauth20/server/utils/UriUtils.java
+++ src/main/java/com/brew/oauth20/server/utils/UriUtils.java
@@ -1,6 +1,5 @@
 package com.brew.oauth20.server.utils;
 
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
 
@@ -10,9 +9,7 @@ public class UriUtils {
 
     public static boolean isValidUrl(String url) throws PatternSyntaxException, NullPointerException {
         try {
-            Pattern regex = Pattern.compile("\\b(?:(https?|ftp|file)://|www\\.)?[-A-Z0-9+&#/%?=~_|$!:,.;]*[A-Z0-9+&@#/%=~_|$]\\.[-A-Z0-9+&@#/%?=~_|$!:,.;]*[A-Z0-9+&@#/%=~_|$]", Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE);
-            Matcher regexMatcher = regex.matcher(url);
-            return regexMatcher.matches();
+            return java.util.regex.Pattern.compile("\\b(?:(https?|ftp|file)://|www\\.)?[-A-Z0-9+&#/%?=~_|$!:,.;]*[A-Z0-9+&@#/%=~_|$]\\.[-A-Z0-9+&@#/%?=~_|$!:,.;]*[A-Z0-9+&@#/%=~_|$]", Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE).matcher(url).matches();
         } catch (PatternSyntaxException | NullPointerException ex) {
             return false;
         }
