ccbe68e6f691b53075a7dc06436dfdc709e78681
Mehmet Fahri Özmen <mehmetfahriozmen@gmail.com>
2023-03-31 19:08:01 +0300

    feat(TMID:104): refactored with sonar warnings.

--- src/main/java/com/brew/oauth20/server/service/impl/JwtServiceImpl.java
+++ src/main/java/com/brew/oauth20/server/service/impl/JwtServiceImpl.java
@@ -6,6 +6,8 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
+import javax.crypto.spec.SecretKeySpec;
+import java.security.Key;
 import java.time.Instant;
 import java.util.Date;
 import java.util.Map;
@@ -13,13 +15,15 @@ import java.util.Map;
 public class JwtServiceImpl implements JwtService {
     @Override
     public TokenModel signToken(String subject, String audience, String issuerUri, String state, Integer tokenExpiresInMinutes, String signingKey, Map<String, Object> additionalClaims) {
+        var expiresInSeconds = tokenExpiresInMinutes * 60L;
+
         // create claims for JWT token
         Claims claims = Jwts.claims().
                 setSubject(subject).
                 setAudience(audience).
                 setIssuer(issuerUri).
                 setIssuedAt(Date.from(Instant.now())).
-                setExpiration(Date.from(Instant.now().plusSeconds(tokenExpiresInMinutes * 60)));
+                setExpiration(Date.from(Instant.now().plusSeconds(expiresInSeconds)));
         if (additionalClaims != null) {
             additionalClaims.forEach(claims::put);
         }
@@ -27,15 +31,20 @@ public class JwtServiceImpl implements JwtService {
         // sign JWT token
         var token = Jwts.builder()
                 .setClaims(claims)
-                .signWith(SignatureAlgorithm.HS256, signingKey)
+                .signWith(getSigningKey(signingKey))
                 .compact();
 
         // create token model object and return
         return TokenModel.builder()
                 .accessToken(token)
                 .tokenType("Bearer")
-                .expiresIn(tokenExpiresInMinutes * 60L)
+                .expiresIn(expiresInSeconds)
                 .state(state)
                 .build();
+    }
+
+    private Key getSigningKey(String signingKey) {
+        byte[] keyBytes = signingKey.getBytes();
+        return new SecretKeySpec(keyBytes, SignatureAlgorithm.HS256.getJcaName());
     }
 }
