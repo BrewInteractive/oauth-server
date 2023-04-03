e029a423ee07a40720552ef625aefdff9c6bacf9
Ali Torun <toruna@gmail.com>
2023-04-03 16:22:53 +0300

    feat(TMID-81): remove sample components

--- src/main/java/com/brew/oauth20/server/model/GreetingModel.java
+++
@@ -1,8 +0,0 @@
-package com.brew.oauth20.server.model;
-
-import lombok.Builder;
-
-
-@Builder
-public record GreetingModel(long id, String content) {
-}
--- src/main/java/com/brew/oauth20/server/service/GreetingService.java
+++
@@ -1,7 +0,0 @@
-package com.brew.oauth20.server.service;
-
-import com.brew.oauth20.server.model.GreetingModel;
-
-public interface GreetingService {
-    GreetingModel getGreetingModel(String name);
-}
