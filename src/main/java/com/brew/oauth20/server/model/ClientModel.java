948e8cf7faea36e98d57ae1358963ae65f01bd55
Mehmet Fahri Özmen <mehmetfahriozmen@gmail.com>
2023-04-05 17:30:13 +0300

    feat(TMID-107, TMID-109): implemented TokenService.

--- src/main/java/com/brew/oauth20/server/model/ClientModel.java
+++ src/main/java/com/brew/oauth20/server/model/ClientModel.java
@@ -3,14 +3,24 @@ package com.brew.oauth20.server.model;
 import lombok.Builder;
 
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.UUID;
-
 
 @Builder
 public record ClientModel(
         UUID id,
+        String clientId,
+        String clientSecret,
+        String audience,
+        String issuerUri,
         Boolean issueRefreshTokens,
+        int tokenExpiresInMinutes,
+        int refreshTokenExpiresInDays,
         ArrayList<GrantModel> grantList,
         ArrayList<RedirectUriModel> redirectUriList
 ) {
+    public String clientSecretDecoded() {
+        byte[] decodedBytes = Base64.getUrlDecoder().decode(clientSecret);
+        return new String(decodedBytes);
+    }
 }
