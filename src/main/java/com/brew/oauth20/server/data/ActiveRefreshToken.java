82c5bfd94c2c3cd21d7ca401d60b09fe3c988da2
erailea <toruna@gmail.com>
2023-04-05 17:57:26 +0300

    refactor: remove fields ActiveRefreshToken not has

--- src/main/java/com/brew/oauth20/server/data/ActiveRefreshToken.java
+++ src/main/java/com/brew/oauth20/server/data/ActiveRefreshToken.java
@@ -7,9 +7,7 @@ import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
 import java.time.OffsetDateTime;
-import java.util.LinkedHashSet;
 import java.util.Objects;
-import java.util.Set;
 import java.util.UUID;
 
 @Getter
@@ -21,9 +19,6 @@ import java.util.UUID;
 @Entity
 @Table(name = "refresh_tokens__active")
 public class ActiveRefreshToken {
-    @OneToMany(mappedBy = "replacedByToken")
-    @ToString.Exclude
-    private final Set<ActiveRefreshToken> refreshTokens = new LinkedHashSet<>();
     @Id
     @Column(name = "id", nullable = false)
     private UUID id;
@@ -31,18 +26,8 @@ public class ActiveRefreshToken {
     private OffsetDateTime createdAt;
     @Column(name = "updated_at", nullable = false)
     private OffsetDateTime updatedAt;
-    @Column(name = "expires_at", nullable = false)
-    private OffsetDateTime expiresAt;
     @Column(name = "token", nullable = false, length = Integer.MAX_VALUE)
     private String token;
-    @Column(name = "revoked_at", nullable = false)
-    private OffsetDateTime revokedAt;
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "replaced_by_token_id", nullable = false)
-    @ToString.Exclude
-    private ActiveRefreshToken replacedByToken;
-    @Column(name = "user_id", nullable = false)
-    private Long userId;
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @OnDelete(action = OnDeleteAction.CASCADE)
     @JoinColumn(name = "client_user_id", nullable = false)
