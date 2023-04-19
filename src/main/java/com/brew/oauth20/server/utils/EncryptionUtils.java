ade6fc7daf95e5fe4b6b9940a4b4e19ab590ffde
Mehmet Fahri Özmen <mehmetfahriozmen@gmail.com>
2023-04-19 18:43:58 +0300

    style: throw dedicated exceptions.

--- src/main/java/com/brew/oauth20/server/utils/EncryptionUtils.java
+++ src/main/java/com/brew/oauth20/server/utils/EncryptionUtils.java
@@ -2,9 +2,10 @@ package com.brew.oauth20.server.utils;
 
 import org.apache.tomcat.util.codec.binary.Base64;
 
-import javax.crypto.Cipher;
-import javax.crypto.SecretKey;
+import javax.crypto.*;
 import javax.crypto.spec.SecretKeySpec;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 
 
 public class EncryptionUtils {
@@ -12,7 +13,7 @@ public class EncryptionUtils {
         throw new IllegalStateException("Utility class");
     }
 
-    public static String encrypt(String data, String algorithm, String key) throws Exception {
+    public static String encrypt(String data, String algorithm, String key) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
         SecretKey secretKey = new SecretKeySpec(key.getBytes(), algorithm);
         Cipher cipher = Cipher.getInstance(algorithm);
         cipher.init(Cipher.ENCRYPT_MODE, secretKey);
@@ -20,7 +21,7 @@ public class EncryptionUtils {
         return Base64.encodeBase64String(encryptedData);
     }
 
-    public static String decrypt(String encryptedData, String algorithm, String key) throws Exception {
+    public static String decrypt(String encryptedData, String algorithm, String key) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
         SecretKey secretKey = new SecretKeySpec(key.getBytes(), algorithm);
         Cipher cipher = Cipher.getInstance(algorithm);
         cipher.init(Cipher.DECRYPT_MODE, secretKey);
