5dd16902346322c1a9afe631907d8e2f46722317
Ali Torun <toruna@gmail.com>
2023-07-14 12:36:14 +0300

    fix(TMID-351): add new illegal argument cases

--- src/test/java/com/brew/oauth20/server/provider/filestorage/FileStorageProviderS3Test.java
+++ src/test/java/com/brew/oauth20/server/provider/filestorage/FileStorageProviderS3Test.java
@@ -8,6 +8,9 @@ import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,6 +18,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.stream.Stream;
 
 import static org.mockito.Mockito.*;
 
@@ -29,6 +33,28 @@ class FileStorageProviderS3Test {
     @InjectMocks
     private S3StorageProvider s3StorageProvider;
 
+    private static Stream<Arguments> store_should_throw_illegal_argument_exception() {
+        byte[] sampleByteArray = new byte[]{1, 2, 3, 4, 5};
+
+        return Stream.of(
+                Arguments.of(
+                        null,
+                        "path/to/file.txt"
+                ),
+                Arguments.of(
+                        new byte[0],
+                        "path/to/file.txt"
+                ),
+                Arguments.of(
+                        sampleByteArray,
+                        ""
+                ),
+                Arguments.of(
+                        sampleByteArray,
+                        null
+                )
+        );
+    }
 
     @Test
     void store_should_upload_file_to_s3_and_return_presigned_url() throws IOException {
@@ -50,23 +76,9 @@ class FileStorageProviderS3Test {
         Assertions.assertEquals(expectedUrl, result);
     }
 
-    @Test
-    void store_should_throw_exception_when_file_bytes_are_missing_or_empty() {
-        // Arrange
-        byte[] fileBytes = null;
-        String filePath = "path/to/file.txt";
-
-        // Act and Assert
-        Assertions.assertThrows(IllegalArgumentException.class,
-                () -> s3StorageProvider.store(fileBytes, filePath));
-    }
-
-    @Test
-    void store_should_throw_exception_when_file_path_is_missing_or_empty() {
-        // Arrange
-        byte[] fileBytes = {1, 2, 3};
-        String filePath = null;
-
+    @MethodSource
+    @ParameterizedTest
+    void store_should_throw_illegal_argument_exception(byte[] fileBytes, String filePath) {
         // Act and Assert
         Assertions.assertThrows(IllegalArgumentException.class,
                 () -> s3StorageProvider.store(fileBytes, filePath));
