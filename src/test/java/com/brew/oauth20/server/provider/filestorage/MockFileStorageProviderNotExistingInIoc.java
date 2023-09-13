976cc0b46897f882e68fbc67a4c44cc0fb1ee552
yasin <yasin@brewww.com>
2023-09-13 17:46:16 +0300

    fix(TMID-537): remove unnecessary client upload endpoint, aws, s3 dependency, file upload providers

--- src/test/java/com/brew/oauth20/server/provider/filestorage/MockFileStorageProviderNotExistingInIoc.java
+++
@@ -1,9 +0,0 @@
-package com.brew.oauth20.server.provider.filestorage;
-
-public class MockFileStorageProviderNotExistingInIoc extends BaseFileStorageProvider {
-
-    @Override
-    public String store(byte[] fileBytes, String filePath) {
-        return null;
-    }
-}
