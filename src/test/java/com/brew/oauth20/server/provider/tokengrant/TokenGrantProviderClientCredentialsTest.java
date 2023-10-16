0b1089a8e749b13254ea7349a9e97d1a1f059679
Ali Torun <toruna@gmail.com>
2023-10-16 16:05:08 +0300

    test: add TokenGrantProviderClientCredentials clients users not found case

--- src/test/java/com/brew/oauth20/server/provider/tokengrant/TokenGrantProviderClientCredentialsTest.java
+++ src/test/java/com/brew/oauth20/server/provider/tokengrant/TokenGrantProviderClientCredentialsTest.java
@@ -1,14 +1,19 @@
 package com.brew.oauth20.server.provider.tokengrant;
 
 import com.brew.oauth20.server.data.enums.GrantType;
+import com.brew.oauth20.server.exception.ClientsUserNotFoundException;
 import com.brew.oauth20.server.fixture.ClientModelFixture;
 import com.brew.oauth20.server.fixture.TokenRequestModelFixture;
-import com.brew.oauth20.server.model.*;
+import com.brew.oauth20.server.model.ClientModel;
+import com.brew.oauth20.server.model.TokenModel;
+import com.brew.oauth20.server.model.TokenRequestModel;
+import com.brew.oauth20.server.model.TokenResultModel;
 import com.brew.oauth20.server.service.ClientService;
 import com.brew.oauth20.server.service.TokenService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -103,6 +108,44 @@ class TokenGrantProviderClientCredentialsTest {
         when(tokenService.generateToken(clientModel, tokenRequest.state, tokenRequest.additional_claims))
                 .thenReturn(tokenResultModel.getResult());
 
+        // Act
+        var result = tokenGrantProviderClientCredentials.generateToken(authorizationHeader, tokenRequest);
+
+        // Assert
+        assertThat(result).isEqualTo(tokenResultModel);
+    }
+
+    @Test
+    void should_return_unauthorized_client_clients_user_not_found_test() {
+        // Arrange
+        var clientModelFixture = new ClientModelFixture();
+
+        var clientModel = clientModelFixture.createRandomOne(1, new GrantType[]{GrantType.client_credentials});
+
+        var tokenRequestFixture = new TokenRequestModelFixture();
+
+        var tokenRequest = tokenRequestFixture.createRandomOne(new GrantType[]{GrantType.client_credentials});
+        tokenRequest.setClient_id(clientModel.clientId());
+        tokenRequest.setClient_secret(clientModel.clientSecret());
+
+        var tokenRequestWithoutClient = tokenRequestFixture.createRandomOne(new GrantType[]{GrantType.client_credentials});
+        tokenRequestWithoutClient.setClient_id("");
+        tokenRequestWithoutClient.setClient_secret("");
+
+        String authorizationHeader = Base64.getEncoder().withoutPadding().encodeToString((clientModel.clientId() + ":" + clientModel.clientSecret()).getBytes());
+
+        var clientCredentialsPair = Pair.of(clientModel.clientId(), clientModel.clientSecret());
+
+        when(clientService.getClient(tokenRequest.client_id, tokenRequest.client_secret))
+                .thenReturn(clientModel);
+
+        when(clientService.decodeClientCredentials(authorizationHeader))
+                .thenReturn(Optional.of(clientCredentialsPair));
+
+        when(tokenService.generateToken(clientModel, tokenRequest.state, tokenRequest.additional_claims))
+                .thenThrow(new ClientsUserNotFoundException(clientModel.clientId(), "user"));
+
+        var tokenResultModel = new TokenResultModel(null, "unauthorized_client");
         // Act
         var result = tokenGrantProviderClientCredentials.generateToken(authorizationHeader, tokenRequest);
 
