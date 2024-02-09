934aefa1b8f92027743a76c5c76f62aa65f3b2e7
Mehmet Fahri Özmen <mehmetfahriozmen@gmail.com>
2024-02-09 17:52:26 +0300

    refactor: removed length attributes.

--- src/main/java/com/brew/oauth20/server/data/AuthorizationCode.java
+++ src/main/java/com/brew/oauth20/server/data/AuthorizationCode.java
@@ -46,7 +46,7 @@ public class AuthorizationCode {
     @Column(name = "updated_at", nullable = false)
     private OffsetDateTime updatedAt;
 
-    @Column(name = "scope", nullable = false, length = Integer.MAX_VALUE)
+    @Column(name = "scope", nullable = false)
     private String scope;
 
     @Override
