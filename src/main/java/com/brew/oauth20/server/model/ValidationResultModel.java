5d5d347b270484e33949c805ba28250a8a977cfb
Mehmet Fahri Özmen <mehmetfahriozmen@gmail.com>
2024-03-28 15:38:40 +0300

    refactor: removed unused models.

--- src/main/java/com/brew/oauth20/server/model/ValidationResultModel.java
+++
@@ -1,8 +0,0 @@
-package com.brew.oauth20.server.model;
-
-public class ValidationResultModel extends BaseResultModel<Boolean> {
-    public ValidationResultModel(Boolean result, String error) {
-        super(result, error);
-    }
-}
-
